f9c0b741bd90066fffe11a61d531ecd65562a697
Doug-oss <douglasj.sjc@gmail.com>
2022-04-06 21:01:34 -0300

    Matriz Identidade

--- Matrizes.js
+++ Matrizes.js
@@ -1,21 +1,28 @@
-class Matrix{
+class Matriz{
     constructor(rows, cols){
         this.rows = rows;
         this.cols = cols;
+        //Preenche a matriz mxn com 0
         this.data = Array(rows).fill().map(() => Array(cols).fill(0));
     }
-    static randomize(matrix){
+    //Randomiza os numeros
+    static random(matrix){
         for(let i = 0; i < matrix.rows; i++){
             for(let j = 0; j < matrix.cols; j++){
                 matrix.data[i][j] =Math.floor(Math.random() * 10);
             }
         }
     }
+    //Matriz Identidade
+    static matrixI(matrix){
+        for(let i = 0; i < matrix.rows; i++){
+            for(let j = 0; j < matrix.cols; j++){
+                if(i == j){
+                    matrix.data[i][j] = 1;
+                }else{
+                    matrix.data[i][j] = 0;
+                }
+            }
+        }
+    }
 }
-
-m1 = new Matrix(2,2);
-m2 = new Matrix(2,2);
-
-m1 = Matrix.randomize(m1);
-m2 = Matrix.randomize(m2);
-
