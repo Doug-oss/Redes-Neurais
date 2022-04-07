e2cf3efa8a90390e3fd7ef9dddda0288bdbb610d
Doug-oss <douglasj.sjc@gmail.com>
2022-04-06 21:26:25 -0300

    Matriz Trasposta

--- Matrizes.js
+++ Matrizes.js
@@ -1,28 +1,40 @@
-class Matriz{
+class Matrix{
+    //Define numero de linhas e colunas
     constructor(rows, cols){
         this.rows = rows;
         this.cols = cols;
-        //Preenche a matriz mxn com 0
+        //Preenche a matriz rXc com 0
         this.data = Array(rows).fill().map(() => Array(cols).fill(0));
     }
     //Randomiza os numeros
-    static random(matrix){
-        for(let i = 0; i < matrix.rows; i++){
-            for(let j = 0; j < matrix.cols; j++){
-                matrix.data[i][j] =Math.floor(Math.random() * 10);
+    static matrixRandom(m){
+        for(let i = 0; i < m.rows; i++){
+            for(let j = 0; j < m.cols; j++){
+                m.data[i][j] =Math.floor(Math.random() * 10);
             }
         }
     }
     //Matriz Identidade
-    static matrixI(matrix){
-        for(let i = 0; i < matrix.rows; i++){
-            for(let j = 0; j < matrix.cols; j++){
+    static matrixIdent(m){
+        for(let i = 0; i < m.rows; i++){
+            for(let j = 0; j < m.cols; j++){
                 if(i == j){
-                    matrix.data[i][j] = 1;
+                    m.data[i][j] = 1;
                 }else{
-                    matrix.data[i][j] = 0;
+                    ma.data[i][j] = 0;
                 }
             }
         }
     }
+    //Matriz Transposta
+    static matrixTranspose(m){
+        let newMatrix = new Matrix(m.cols, m.rows);
+        for(let i = 0; i < m.rows; i++){
+            for(let j = 0; j < m.cols; j++){
+                newMatrix.data[j][i] = m.data[i][j];
+            }
+        }
+        return newMatrix;
+    }
 }
+
